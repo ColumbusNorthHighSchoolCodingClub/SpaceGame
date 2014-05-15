2f4e8a5b1eb370e0983d8f42db2e6265267a2a21
anangrybeaver <guitarplayer19997@gmail.com>
2014-05-15 09:19:09 -0400

    Updated getMousePosition to Stop Causing Errors

--- ArcadeEngine/java/com/arcadeengine/AnimPanel.java
+++ ArcadeEngine/java/com/arcadeengine/AnimPanel.java
@@ -9,15 +9,13 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-
 import javax.swing.JPanel;
-
 import com.arcadeengine.gui.Gui;
 import com.arcadeengine.gui.GuiHandler;
 
 /**
  * Main component for Arcade Engine.
- * 
+ *
  * @author David Baker
  * @version 2.1.1
  */
@@ -25,52 +23,54 @@ import com.arcadeengine.gui.GuiHandler;
 public abstract class AnimPanel extends JPanel implements KeyListener, MouseListener, MouseMotionListener {
 	// Variables
 	private String myName;
-
+	
 	private int timerDelay = 60;
-
+	
 	private int FPS = 0;
 	private long nextFPSTime = 0;
 	private int fpsLoop = 0;
-
+	
 	private int frameNumber;
-
+	
 	private Point lastMouseCoord = new Point(-1, -1);
-
+	
 	// Handlers
 	protected GuiHandler guiHandler;
 	protected KeyBindingHandler kbHandler = new KeyBindingHandler();
-
+	
 	// Booleans
 	private boolean paused = true;
-
+	
 	private boolean componentClicked = false;
-
+	
 	private boolean resizable = false;
-
+	
 	public abstract Graphics renderFrame(Graphics g);
-
+	
 	protected void drawGui(Graphics g) {
-		if (this.guiHandler != null)
+	
+		if(this.guiHandler != null)
 			this.guiHandler.drawGui(g);
 	}
-
+	
 	protected void updateGui() {
-		if (this.guiHandler != null)
+	
+		if(this.guiHandler != null)
 			this.guiHandler.updateGui();
 	}
-
+	
 	public abstract void process();
-
+	
 	public abstract void initRes();
-
+	
 	public String getMyName() {
-
+	
 		return myName;
 	}
-
+	
 	/**
 	 * Constructor for objects of class Game
-	 * 
+	 *
 	 * @param name
 	 *            The name of the AnimPanel.
 	 * @param width
@@ -79,9 +79,10 @@ public abstract class AnimPanel extends JPanel implements KeyListener, MouseList
 	 *            The height (in pixels) of the AnimPanel.
 	 */
 	protected void createInstance(String name, int width, int height) {
+	
 		frameNumber = 0;
 		this.myName = name;
-
+		
 		setPreferredSize(new Dimension(width, height));
 		// this.setSize(new Dimension(width,height));
 		// this.setLocation(80, 80); // move to the right
@@ -90,197 +91,223 @@ public abstract class AnimPanel extends JPanel implements KeyListener, MouseList
 		addKeyListener(this);
 		addMouseListener(this);
 		addMouseMotionListener(this);
-
+		
 		// ---LOAD ALL RESOURCES---
 		initRes();
 	}
-
+	
 	public void createGuiHandler(Gui gui) {
-
+	
 		this.guiHandler = new GuiHandler(this, gui);
 	}
-
+	
 	// Getters
 	public GuiHandler getGuiHandler() {
+	
 		return guiHandler;
 	}
-
+	
 	public KeyBindingHandler getKeyBoardHandler() {
+	
 		return kbHandler;
 	}
-
+	
 	public void setResizable(boolean value) {
-
+	
 		this.resizable = value;
 	}
-
+	
 	public boolean isResizable() {
-
+	
 		return this.resizable;
 	}
-
+	
 	public void calculateRenderFPS() {
-
-		if (System.currentTimeMillis() >= nextFPSTime) {
-
+	
+		if(System.currentTimeMillis() >= nextFPSTime) {
+			
 			FPS = Integer.valueOf(fpsLoop);
-
+			
 			fpsLoop = 0;
-
+			
 			nextFPSTime = System.currentTimeMillis() + 1000L;
-		} else
+		}
+		else
 			fpsLoop++;
 	}
-
+	
 	public void setTimerDelay(int delay) {
+	
 		this.timerDelay = delay;
 	}
-
+	
 	public int getTimerDelay() {
+	
 		return timerDelay;
 	}
-
+	
 	public int getFPS() {
+	
 		return FPS;
 	}
-
+	
 	public int getFrameNumber() {
+	
 		return frameNumber;
 	}
-
+	
 	public boolean isPaused() {
+	
 		return paused;
 	}
-
+	
 	public void setPauseState(boolean state) {
+	
 		paused = state;
 	}
-
+	
 	@Override
 	public void paintComponent(Graphics g) {
+	
 		frameNumber++;
-
-		if (frameNumber >= 20000)
+		
+		if(frameNumber >= 20000)
 			frameNumber = 0;
-
+		
 		this.requestFocusInWindow();
-
+		
 		g.setColor(Color.white);
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
-
+		
 		g = renderFrame(g);
 	}
-
+	
 	@Override
 	public Point getMousePosition() {
-
+	
 		try {
-			Point temp = new Point(super.getMousePosition().x + 1 - 1, super.getMousePosition().y + 1 - 1);
-
-			lastMouseCoord = temp;
-			return super.getMousePosition();
-		} catch (Exception e) {
+			Integer x = super.getMousePosition().x, y = super.getMousePosition().y;
+			
+			if(x == null || y == null)
+				throw new NullPointerException();
+			
+			lastMouseCoord = new Point(x, y);
+			return lastMouseCoord;
+		}
+		catch(Exception e) {
 			try {
-				if (!this.lastMouseCoord.equals(null))
-					return lastMouseCoord;
-			} catch (NullPointerException es) {
+				return lastMouseCoord;
+			}
+			catch(NullPointerException es) {
 			}
 		}
-
+		
 		return new Point(-1, -1);
 	}
-
+	
 	public boolean isComponentClicked() {
-
+	
 		return this.componentClicked;
 	}
-
+	
 	public void setComponentClicked(boolean isClicked) {
-
+	
 		this.componentClicked = isClicked;
 	}
-
+	
 	public boolean isLeftClickHeld() {
+	
 		return this.leftClickHeld;
 	}
-
+	
 	public boolean isRightClickHeld() {
+	
 		return this.rightClickHeld;
 	}
-
+	
 	public boolean isMiddleClickHeld() {
+	
 		return this.middleClickHeld;
 	}
-
-	private boolean leftClickHeld = false, rightClickHeld = false, middleClickHeld = false;
-
+	
+	private boolean leftClickHeld = false, rightClickHeld = false,
+			middleClickHeld = false;
+	
 	@Override
 	public void mousePressed(MouseEvent e) {
-		if (e.getButton() == MouseEvent.BUTTON1)
-			if (leftClickHeld != true)
+	
+		if(e.getButton() == MouseEvent.BUTTON1)
+			if(leftClickHeld != true)
 				leftClickHeld = true;
-
-		if (e.getButton() == MouseEvent.BUTTON2)
-			if (middleClickHeld != true)
+		
+		if(e.getButton() == MouseEvent.BUTTON2)
+			if(middleClickHeld != true)
 				middleClickHeld = true;
-
-		if (e.getButton() == MouseEvent.BUTTON3)
-			if (rightClickHeld != true)
+		
+		if(e.getButton() == MouseEvent.BUTTON3)
+			if(rightClickHeld != true)
 				rightClickHeld = true;
 	}
-
+	
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		if (e.getButton() == MouseEvent.BUTTON1)
-			if (leftClickHeld != false)
+	
+		if(e.getButton() == MouseEvent.BUTTON1)
+			if(leftClickHeld != false)
 				leftClickHeld = false;
-
-		if (e.getButton() == MouseEvent.BUTTON2)
-			if (middleClickHeld != false)
+		
+		if(e.getButton() == MouseEvent.BUTTON2)
+			if(middleClickHeld != false)
 				middleClickHeld = false;
-
-		if (e.getButton() == MouseEvent.BUTTON3)
-			if (rightClickHeld != false)
+		
+		if(e.getButton() == MouseEvent.BUTTON3)
+			if(rightClickHeld != false)
 				rightClickHeld = false;
 	}
-
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
-
-		if (guiHandler != null)
+	
+		if(guiHandler != null)
 			componentClicked = guiHandler.getGui().updateOnClick(e.getButton());
 	}
-
+	
 	@Override
 	public void mouseEntered(MouseEvent e) {
+	
 	}
-
+	
 	@Override
 	public void mouseExited(MouseEvent e) {
-
+	
 	}
-
+	
 	@Override
 	public void keyTyped(KeyEvent arg0) {
+	
 	}
-
+	
 	@Override
 	public void mouseDragged(MouseEvent arg0) {
+	
 	}
-
+	
 	@Override
 	public void mouseMoved(MouseEvent arg0) {
+	
 	}
-
+	
 	@Override
 	public void keyPressed(KeyEvent e) {
+	
 		String key = KeyEvent.getKeyText(e.getKeyCode());
-
+		
 		this.kbHandler.runBindings(key);
 	}
-
+	
 	@Override
 	public void keyReleased(KeyEvent e) {
+	
 		String key = KeyEvent.getKeyText(e.getKeyCode());
 		this.kbHandler.removeKey(key);
 	}
--- ArcadeEngine/java/com/arcadeengine/gui/Gui.java
+++ ArcadeEngine/java/com/arcadeengine/gui/Gui.java
@@ -12,58 +12,58 @@ import com.arcadeengine.AnimPanel;
 
 public abstract class Gui {
 	protected AnimPanel panel;
-	
+
 	private Color bgColor = new Color(68, 68, 68, 160);
-	
-	protected ArrayList<ArrayList<GuiComponent>> componentArrays = new ArrayList<>();
-	protected ArrayList<GuiComponent> components = new ArrayList<>();
-	
+
+	protected ArrayList<ArrayList<GuiComponent>> componentArrays = new ArrayList<ArrayList<GuiComponent>>();
+	protected ArrayList<GuiComponent> components = new ArrayList<GuiComponent>();
+
 	/** The title for each GUI screen **/
 	private String title = "Arcade Engine";
-	
+
 	/** The font the title will be displayed with */
 	private Font titleFont = new Font("Arial", 3, 65);
-	
+
 	/** Color of the title */
 	private Color titleColor = Color.BLUE;
-	
+
 	/** Y Coordinate of the title */
 	private int titleY = 150;
-	
+
 	/** The image in the background of the game. **/
 	private Image background;
-	
+
 	/** The last Gui Visited **/
 	private Gui parent = null;
-	
+
 	public Gui(AnimPanel panel) {
-	
+
 		this.panel = panel;
 		componentArrays.add(components); // Add the default array
 	}
-	
+
 	/**
 	 * Returns the current background image.
 	 */
 	public Image getBGImage() {
-	
+
 		if(background != null)
 			return background;
-		
+
 		return null;
 	}
-	
+
 	public void setBGImage(Image background) {
-	
+
 		if(background != null)
 			this.background = background;
 	}
-	
+
 	/**
 	 * Returns the background color.
 	 */
 	public Color getBGColor() {
-	
+
 		try {
 			return bgColor;
 		}
@@ -71,7 +71,7 @@ public abstract class Gui {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Sets the background color to the given R-G-B code with alpha
 	 * transparency.
@@ -86,10 +86,10 @@ public abstract class Gui {
 	 *            Alpha integer of the given color code.
 	 */
 	public void setBGColor(int r, int g, int b, int a) {
-	
+
 		this.bgColor = new Color(r, g, b, a);
 	}
-	
+
 	/**
 	 * Sets the background to the given color.
 	 *
@@ -97,78 +97,78 @@ public abstract class Gui {
 	 *            The color for the background.
 	 */
 	public void setBGColor(Color color) {
-	
+
 		this.bgColor = color;
 	}
-	
+
 	/**
 	 * Returns the last visited Gui if applicable.
 	 */
 	public Gui getParent() {
-	
+
 		return this.parent;
 	}
-	
+
 	/**
 	 * Usually only used by the GuiHandler class, but feel free to abuse it as
 	 * needed.
 	 */
 	public void setParent(Gui par) {
-	
+
 		this.parent = par;
 	}
-	
+
 	/**
 	 * @return the title
 	 */
 	public String getTitle() {
-	
+
 		return title;
 	}
-	
+
 	/**
 	 * @param title
 	 *            the title to set
 	 */
 	public void setTitle(String title) {
-	
+
 		this.title = title;
 	}
-	
+
 	/**
 	 * @return the titleFont
 	 */
 	public Font getTitleFont() {
-	
+
 		return titleFont;
 	}
-	
+
 	/**
 	 * @param titleFont
 	 *            the titleFont to set
 	 */
 	public void setTitleFont(Font titleFont) {
-	
+
 		this.titleFont = titleFont;
 	}
-	
+
 	/**
 	 * @return the titleColor
 	 */
 	public Color getTitleColor() {
-	
+
 		return titleColor;
 	}
-	
+
 	/**
 	 * @param titleColor
 	 *            the titleColor to set
 	 */
 	public void setTitleColor(Color titleColor) {
-	
+
 		this.titleColor = titleColor;
 	}
-	
+
 	/**
 	 * Draws the title text of the GUi.
 	 *
@@ -178,18 +178,18 @@ public abstract class Gui {
 	 *            The Width of the window.
 	 */
 	protected void drawTitle(Graphics g, int width) {
-	
+
 		Graphics2D page = (Graphics2D) g;
-		
+
 		page.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		
+
 		g.setFont(getTitleFont());
-		
+
 		Rectangle2D rect = page.getFontMetrics().getStringBounds(getTitle(), page);
-		
+
 		this.drawString(this.getTitle(), getTitleFont(), this.getTitleColor(), ((width / 2) - ((int) rect.getWidth() / 2)) + 2, titleY + 2, page);
 	}
-	
+
 	/**
 	 * Draws a given String with a shadow for contrast.
 	 *
@@ -203,39 +203,39 @@ public abstract class Gui {
 	 *            The Graphics object.
 	 */
 	protected void drawString(String str, int x, int y, Graphics g) {
-	
+
 		drawString(str, new Font("Arial", Font.BOLD, 12), x, y, g);
 	}
-	
+
 	protected void drawString(String str, Font font, int x, int y, Graphics g) {
-	
+
 		drawString(str, font, Color.DARK_GRAY, x, y, g);
 	}
-	
-	protected void drawString(String str, Font font, Color color, int x, int y, Graphics g) {
-	
-		Graphics2D page = (Graphics2D) g;
 
+	protected void drawString(String str, Font font, Color color, int x, int y, Graphics g) {
+
+		Graphics2D page = (Graphics2D) g;
+		
 		Font old = g.getFont();
 		g.setFont(font);
-		
+
 		page.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		
+
 		g.setColor(Color.black);
 		//		g.drawString(str, x, y);
 		//
 		//		g.drawString(str, x + 2, y);
-		
+
 		g.drawString(str, x + 1, y + 1);
-		
+
 		//		g.drawString(str, x, y + 2);
-		
+
 		g.setColor(color);
 		g.drawString(str, x, y);
-		
+
 		g.setFont(old);
 	}
-	
+
 	/**
 	 * Draws the buttons that aren't given coordinates at given point.
 	 *
@@ -251,20 +251,20 @@ public abstract class Gui {
 	 *            buttons.
 	 */
 	protected void drawComponents(Graphics g, ArrayList<GuiComponent> buttons, int x, int y) {
-	
+
 		int height = 0;
 		int spacing = 4;
-		
+
 		for(GuiComponent b : buttons)
 			if(b.autoplaced == false)
 				b.draw(g);
 			else {
 				b.draw(x, y + height, g);
-				
+
 				height += b.getHeight() + spacing;
 			}
 	}
-	
+
 	/**
 	 * Draws the buttons that aren't given coordinates at given point.
 	 *
@@ -280,10 +280,10 @@ public abstract class Gui {
 	 *            buttons.
 	 */
 	protected void drawComponents(Graphics g, int x, int y) {
-	
+
 		this.drawComponents(g, components, x, y);
 	}
-	
+
 	/**
 	 * Used for highlighting the buttons.
 	 *
@@ -291,13 +291,13 @@ public abstract class Gui {
 	 *            The Mouse Coordinate
 	 */
 	protected void updateComponents(ArrayList<GuiComponent> comps) {
-	
+
 		for(GuiComponent b : comps) {
 			b.onUpdateDefault(panel.getMousePosition());
 			b.update();
 		}
 	}
-	
+
 	/**
 	 * Used for highlighting the buttons.
 	 *
@@ -305,10 +305,10 @@ public abstract class Gui {
 	 *            The Mouse Coordinate
 	 */
 	protected void updateComponents() {
-	
+
 		this.updateComponents(components);
 	}
-
+	
 	/**
 	 * Draw the Gui onto the Screen.
 	 *
@@ -318,7 +318,7 @@ public abstract class Gui {
 	 *            The game. (put "this" here)
 	 */
 	public abstract void drawGui(Graphics g);
-	
+
 	/**
 	 * Updates for the GUI.
 	 *
@@ -326,13 +326,13 @@ public abstract class Gui {
 	 *            The game. (put "this" here)
 	 */
 	public abstract void updateGui();
-	
+
 	/**
 	 *
 	 * @param btn
 	 */
 	public final boolean updateOnClick(int btn) {
-
+	
 		for(ArrayList<GuiComponent> array : componentArrays) {
 			for(GuiComponent component : array) {
 				if(component.isHovered()) {
@@ -343,6 +343,6 @@ public abstract class Gui {
 		}
 		return false;
 	}
-
+	
 	public abstract void actionPerformed(GuiComponent comp, int msBtn);
 }
