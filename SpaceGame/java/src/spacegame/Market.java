81899dbe60c6338f4492110cf26ce204b914dfbb
anangrybeaver <guitarplayer19997@gmail.com>
2014-05-20 12:52:21 -0400

    Update to Client

--- SpaceGame/java/src/spacegame/Market.java
+++ SpaceGame/java/src/spacegame/Market.java
@@ -1,33 +1,47 @@
 package src.spacegame;
 
+import src.spacegame.client.ClientInfo;
+import src.spacegame.client.ClientMain;
+
 public class Market implements Packable {
-	
+
 	private static final String HEADER_CLASS = "MARK";
-	
+
 	private final char PARSE_CHAR = '*';
-	
+
 	private final int fuelMultiplier = 50, matMultiplier = 50;
+
+	private ClientMain clMain;
+	private ClientInfo clInfo;
+
+	public Market(ClientMain clMain) {
 	
-	public Market() {
+		this.clMain = clMain;
+		clInfo = clMain.getClientInfo();
 
 	}
-	
-	public static String getHeader() {
 
+	public static String getHeader() {
+	
 		return HEADER_CLASS;
 	}
+
+	public boolean requestShipment() {
 	
+		return false;
+	}
+
 	@Override
 	public String pack() {
-	
-		String pack = HEADER_CLASS + PARSE_CHAR;
 
+		String pack = HEADER_CLASS + PARSE_CHAR;
+		
 		return pack;
 	}
-	
+
 	@Override
 	public void unpack(String data) {
-	
+
 	}
-	
+
 }
--- SpaceGame/java/src/spacegame/client/gui/GuiDebug.java
+++ SpaceGame/java/src/spacegame/client/gui/GuiDebug.java
@@ -14,61 +14,60 @@ import com.arcadeengine.gui.Gui;
 import com.arcadeengine.gui.GuiComponent;
 
 public class GuiDebug extends Gui {
-
+	
 	private ClientMain clMain = (ClientMain) panel;
-
+	
 	private String addBreak = "----------------------";
 	private String debug[];
-
+	
 	public GuiDebug(AnimPanel panel) {
-
+	
 		super(panel);
-
+		
 		debug = new String[] { "Loading Debug..." };
 	}
-
+	
 	@Override
 	public void drawGui(Graphics g) {
-
+	
 		Graphics2D page = (Graphics2D) g;
-
+		
 		int height = -4;
 		int spacing = -2;
-
+		
 		final Font font = new Font("Consolas", Font.BOLD, 13);
-
+		
 		for(String str : debug) {
 			g.setFont(font);
 			page.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
+			
 			Rectangle2D rect = g.getFontMetrics().getStringBounds(str, g);
-
+			
 			if(font.getSize() <= 20)
 				height += rect.getHeight() + 1;
 			else
 				height += rect.getHeight() + 2;
-
+			
 			g.setColor(new Color(85, 85, 85, 190));
-
+			
 			this.drawString(str, font, Color.WHITE, this.panel.getWidth() - (int) rect.getWidth() - 3, height, g);
-
+			
 			height += spacing;
 		}
 	}
-
+	
 	@Override
 	public void updateGui() {
-
+	
 		String ip = "null";
-
+		
 		try {
 			InetAddress addr = InetAddress.getLocalHost();
 			ip = addr.getHostAddress();
-			
 		}
 		catch(UnknownHostException e) {
 		}
-
+		
 		// All of the info to be displayed on the screen if debug is on.
 		debug = new String[] {
 				"FPS: " + clMain.getFPS(),
@@ -89,9 +88,9 @@ public class GuiDebug extends Gui {
 				"BG Alpha: " + clMain.getGuiHandler().getBGColor().getAlpha(),
 		};
 	}
-	
+
 	@Override
 	public void actionPerformed(GuiComponent btn, int msBtn) {
-
+	
 	}
 }
