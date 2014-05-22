b4f322a2c485bd90adefd7415162f4387c3007f9
anangrybeaver <guitarplayer19997@gmail.com>
2014-05-22 13:09:40 -0400

    Added Separate Class for Storing PlayerStats within ClientInfo

--- SpaceGame/java/src/spacegame/client/ClientInfo.java
+++ SpaceGame/java/src/spacegame/client/ClientInfo.java
@@ -6,40 +6,50 @@ import java.util.ArrayList;
 import src.spacegame.Market;
 import src.spacegame.Packable;
 import src.spacegame.ParseUtil;
+import src.spacegame.PlayerStats;
 import src.spacegame.Ship;
 import src.spacegame.Universe;
 
 public class ClientInfo implements Packable {
-
+	
 	private static final String HEADER_CLASS = "CNFO";
-
+	
 	private final char PARSE_CHAR = '$';
 	private final String headerIP = "MYIP",
 			headerName = "NAME",
-			headerID = "MYID",
-			headerMoney = "CASH";
-
+			headerID = "MYID";
+	
 	private ClientMain clMain; //Do Not Pack
-
+	
 	//TODO: Alliances
-
-	//TODO: Home Planet
 	private Universe univ;
-	private ArrayList<Ship> myShips;
-
 	private Market market;
 
-	private int fuelPurchased = 0, matPurchased = 0, fuelSold = 0, matSold = 0;
-
-	private int myFuel = -1, myMat = -1, myMoney = -1;
+	private PlayerStats myStats;
+	private ArrayList<Ship> myShips;
 	
 	private String myIP = "null";
 	private String clName = "Joe Smith";
 	private int clID = -1;
-
+	
 	public ClientInfo(ClientMain clMain) {
-
+	
+		this();
+		
 		this.clMain = clMain;
+	}
+	
+	public ClientInfo(String data) {
+
+		this();
+		
+		this.unpack(data);
+	}
+	
+	public ClientInfo() {
+
+		this.myStats = new PlayerStats();
+		this.myShips = new ArrayList<Ship>();
 		
 		InetAddress addr = null;
 		try {
@@ -47,135 +57,76 @@ public class ClientInfo implements Packable {
 		}
 		catch(UnknownHostException e) {
 		}
-		
+
 		myIP = addr.getHostAddress();
-
-		myShips = new ArrayList<Ship>();
 	}
 
-	public ClientInfo(String data) {
-	
-		this.myShips = new ArrayList<Ship>();
-
-		this.unpack(data);
-	}
-
-	public ClientInfo() {
-	
-	}
-	
 	public void init() throws UnknownHostException {
-	
+
 		this.clMain.getClientComm().addMessage("LOGI" + PARSE_CHAR + clName);
 	}
-
+	
 	public static String getHeader() {
-
+	
 		return HEADER_CLASS;
 	}
-
+	
 	public String getName() {
-
+	
 		return clName;
 	}
-	
-	public void setName(String str) {
 
+	public void setName(String str) {
+	
 		if(str == null)
 			return;
-		
+
 		if(str.length() < 3)
 			return;
-		
+
 		this.clName = str;
 	}
-
+	
 	public int getID() {
-
+	
 		return clID;
 	}
-
-	public String getIP() {
 	
+	public String getIP() {
+
 		return myIP;
 	}
-
-	public int getFuel() {
-
-		return myFuel;
-	}
 	
-	public int getMat() {
-
-		return myMat;
-	}
-	
-	public int myMoney() {
-
-		return myMoney;
-	}
-	
-	public void updateFuel(int amount) {
-
-		this.myFuel += amount;
-	}
-	
-	public void updateMat(int amount) {
-
-		this.myMat += amount;
-	}
-	
-	public void updateMoney(int amount) {
-
-		this.myMoney += amount;
-	}
-
-	public void setFuel(int amount) {
-	
-		this.myFuel = amount;
-	}
-
-	public void setMat(int amount) {
-	
-		this.myMat = amount;
-	}
-
-	public void setMoney(int amount) {
-	
-		this.myMoney = amount;
-	}
-
 	@Override
 	public String pack() {
-
+	
 		String pack = HEADER_CLASS + PARSE_CHAR;
-
+		
 		pack += univ.pack() + PARSE_CHAR;
-
+		
 		for(Ship ship : myShips)
 			pack += ship.pack() + PARSE_CHAR;
-
+		
 		pack += headerIP + myIP + PARSE_CHAR;
 		pack += headerName + clName + PARSE_CHAR;
 		pack += headerID + clID + PARSE_CHAR;
-		pack += headerMoney + myMoney + PARSE_CHAR;
 
 		return pack;
 	}
-
+	
 	@Override
 	public void unpack(String data) {
-
+	
 		ArrayList<String> parse = ParseUtil.parseString(data, PARSE_CHAR);
-		
+
 		if(!parse.get(0).equals(HEADER_CLASS))
 			return;
-		
-		myShips.clear();
 
+		myShips.clear();
+		
 		for(String str : parse) {
 			String subheader = str.substring(0, 4), info = str.substring(4);
-			
+
 			if(subheader.equals(Universe.getHeader()))
 				univ.unpack(info);
 			else if(subheader.equals(Ship.getHeader()))
@@ -186,12 +137,10 @@ public class ClientInfo implements Packable {
 				clName = info;
 			else if(subheader.equals(headerID))
 				clID = Integer.parseInt(info);
-			else if(subheader.equals(headerMoney))
-				myMoney = Integer.parseInt(info);
 			else if(!subheader.equals(HEADER_CLASS))
 				System.out.println("Unknown Packed Info in ClientInfo");
-
+			
 		}
-
+		
 	}
 }
