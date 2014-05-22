412bff5aea7bf02acb2b67ebd7400ef499b05af3
anangrybeaver <guitarplayer19997@gmail.com>
2014-05-22 14:57:34 -0400

    Updated Packing/Unpacking for PlayerStats and ClientInfo

--- SpaceGame/java/src/spacegame/PlayerStats.java
+++ SpaceGame/java/src/spacegame/PlayerStats.java
@@ -1,11 +1,13 @@
 package src.spacegame;
 
+import java.util.ArrayList;
+
 public class PlayerStats implements Packable {
-	
+
 	private static final String HEADER_CLASS = "STAT";
-	
+
 	private final char PARSE_CHAR = '∑';
-	
+
 	private final String headerFuelPurch = "FPUR",
 			headerMatPurch = "MPUR",
 			headerFuelSold = "FSLD",
@@ -17,89 +19,122 @@ public class PlayerStats implements Packable {
 			headerFuel = "FUEL",
 			headerMat = "MATS",
 			headerMoney = "CASH";
-
+	
 	//TODO: Home Planet
-
+	
 	private int fuelPurchased = 0, matPurchased = 0, fuelSold = 0, matSold = 0;
-
+	
 	private int fuelSpeed = 1, miningSpeed = 1, stealthStat = 1, sensorStat = 1;
-
+	
 	private int myFuel = -1, myMat = -1, myMoney = -1;
-
+	
 	public PlayerStats() {
-	
+
 	}
-	
+
 	public int getFuel() {
-	
+
 		return myFuel;
 	}
-
-	public int getMat() {
 	
+	public int getMat() {
+
 		return myMat;
 	}
-
-	public int myMoney() {
 	
+	public int myMoney() {
+
 		return myMoney;
 	}
-
-	public void updateFuel(int amount) {
 	
+	public void updateFuel(int amount) {
+
 		this.myFuel += amount;
 	}
-
-	public void updateMat(int amount) {
 	
+	public void updateMat(int amount) {
+
 		this.myMat += amount;
 	}
-
-	public void updateMoney(int amount) {
 	
+	public void updateMoney(int amount) {
+
 		this.myMoney += amount;
 	}
-	
-	public void setFuel(int amount) {
 
+	public void setFuel(int amount) {
+	
 		this.myFuel = amount;
 	}
-	
-	public void setMat(int amount) {
 
+	public void setMat(int amount) {
+	
 		this.myMat = amount;
 	}
-	
-	public void setMoney(int amount) {
 
+	public void setMoney(int amount) {
+	
 		this.myMoney = amount;
 	}
-	
+
 	@Override
 	public String pack() {
-
+	
 		String pack = HEADER_CLASS + PARSE_CHAR;
-
+		
 		pack += headerFuel + myFuel + PARSE_CHAR;
 		pack += headerMat + myMat + PARSE_CHAR;
 		pack += headerMoney + myMoney + PARSE_CHAR;
-
+		
 		pack += headerFuelSpeed + fuelSpeed + PARSE_CHAR;
 		pack += headerMiningSpeed + miningSpeed + PARSE_CHAR;
 		pack += headerStealthStat + stealthStat + PARSE_CHAR;
 		pack += headerSensorStat + sensorStat + PARSE_CHAR;
-
+		
 		pack += headerFuelPurch + fuelPurchased + PARSE_CHAR;
 		pack += headerMatPurch + matPurchased + PARSE_CHAR;
 		pack += headerFuelSold + fuelSold + PARSE_CHAR;
 		pack += headerMatSold + matSold + PARSE_CHAR;
-
+		
 		return pack;
 	}
-	
+
 	@Override
 	public void unpack(String data) {
-	
-	}
 
+		ArrayList<String> parse = ParseUtil.parseString(data, PARSE_CHAR);
+		
+		if(!parse.get(0).equals(HEADER_CLASS))
+			return;
+		
+		for(String str : parse) {
+			String subheader = str.substring(0, 4), info = str.substring(4);
+
+			if(subheader.equals(headerFuel))
+				myFuel = Integer.parseInt(subheader);
+			else if(subheader.equals(headerMat))
+				myMat = Integer.parseInt(subheader);
+			else if(subheader.equals(headerMoney))
+				myMoney = Integer.parseInt(subheader);
+			else if(subheader.equals(headerFuelSpeed))
+				fuelSpeed = Integer.parseInt(subheader);
+			else if(subheader.equals(headerMiningSpeed))
+				miningSpeed = Integer.parseInt(subheader);
+			else if(subheader.equals(headerStealthStat))
+				stealthStat = Integer.parseInt(subheader);
+			else if(subheader.equals(headerSensorStat))
+				sensorStat = Integer.parseInt(subheader);
+			else if(subheader.equals(headerFuelPurch))
+				fuelPurchased = Integer.parseInt(subheader);
+			else if(subheader.equals(headerMatPurch))
+				matPurchased = Integer.parseInt(subheader);
+			else if(subheader.equals(headerFuelSold))
+				fuelSold = Integer.parseInt(subheader);
+			else if(subheader.equals(headerMatSold))
+				matSold = Integer.parseInt(subheader);
+			else if(!subheader.equals(HEADER_CLASS))
+				System.out.println("Unknown Packed Info in ClientInfo");
+		}
+	}
+	
 }
--- SpaceGame/java/src/spacegame/client/ClientInfo.java
+++ SpaceGame/java/src/spacegame/client/ClientInfo.java
@@ -11,122 +11,124 @@ import src.spacegame.Ship;
 import src.spacegame.Universe;
 
 public class ClientInfo implements Packable {
-	
+
 	private static final String HEADER_CLASS = "CNFO";
-	
+
 	private final char PARSE_CHAR = '$';
 	private final String headerIP = "MYIP",
 			headerName = "NAME",
-			headerID = "MYID";
-	
+			headerID = "MYID",
+			headerPlayerStats = "PLAY";
+
 	private ClientMain clMain; //Do Not Pack
-	
+
 	//TODO: Alliances
 	private Universe univ;
 	private Market market;
-
+	
 	private PlayerStats myStats;
 	private ArrayList<Ship> myShips;
-	
+
 	private String myIP = "null";
 	private String clName = "Joe Smith";
 	private int clID = -1;
-	
+
 	public ClientInfo(ClientMain clMain) {
-	
+
 		this();
-		
+
 		this.clMain = clMain;
 	}
-	
-	public ClientInfo(String data) {
 
+	public ClientInfo(String data) {
+	
 		this();
-		
+
 		this.unpack(data);
 	}
-	
-	public ClientInfo() {
 
+	public ClientInfo() {
+	
 		this.myStats = new PlayerStats();
 		this.myShips = new ArrayList<Ship>();
-		
+
 		InetAddress addr = null;
 		try {
 			addr = InetAddress.getLocalHost();
 		}
 		catch(UnknownHostException e) {
 		}
-
+		
 		myIP = addr.getHostAddress();
 	}
-
+	
 	public void init() throws UnknownHostException {
-
+	
 		this.clMain.getClientComm().addMessage("LOGI" + PARSE_CHAR + clName);
 	}
-	
+
 	public static String getHeader() {
-	
+
 		return HEADER_CLASS;
 	}
-	
+
 	public String getName() {
-	
+
 		return clName;
 	}
-
-	public void setName(String str) {
 	
+	public void setName(String str) {
+
 		if(str == null)
 			return;
-
+		
 		if(str.length() < 3)
 			return;
-
+		
 		this.clName = str;
 	}
-	
+
 	public int getID() {
-	
+
 		return clID;
 	}
-	
-	public String getIP() {
 
+	public String getIP() {
+	
 		return myIP;
 	}
-	
+
 	@Override
 	public String pack() {
-	
+
 		String pack = HEADER_CLASS + PARSE_CHAR;
-		
+
 		pack += univ.pack() + PARSE_CHAR;
-		
+
 		for(Ship ship : myShips)
 			pack += ship.pack() + PARSE_CHAR;
-		
+
 		pack += headerIP + myIP + PARSE_CHAR;
 		pack += headerName + clName + PARSE_CHAR;
 		pack += headerID + clID + PARSE_CHAR;
-
+		pack += headerPlayerStats + myStats.pack() + PARSE_CHAR;
+		
 		return pack;
 	}
-	
+
 	@Override
 	public void unpack(String data) {
-	
-		ArrayList<String> parse = ParseUtil.parseString(data, PARSE_CHAR);
 
+		ArrayList<String> parse = ParseUtil.parseString(data, PARSE_CHAR);
+		
 		if(!parse.get(0).equals(HEADER_CLASS))
 			return;
-
-		myShips.clear();
 		
+		myShips.clear();
+
 		for(String str : parse) {
 			String subheader = str.substring(0, 4), info = str.substring(4);
-
+			
 			if(subheader.equals(Universe.getHeader()))
 				univ.unpack(info);
 			else if(subheader.equals(Ship.getHeader()))
@@ -137,10 +139,12 @@ public class ClientInfo implements Packable {
 				clName = info;
 			else if(subheader.equals(headerID))
 				clID = Integer.parseInt(info);
+			else if(subheader.equals(headerPlayerStats))
+				myStats.unpack(info);
 			else if(!subheader.equals(HEADER_CLASS))
 				System.out.println("Unknown Packed Info in ClientInfo");
-			
+
 		}
-		
+
 	}
 }
