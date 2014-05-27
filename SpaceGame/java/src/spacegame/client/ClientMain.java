2c6a1f71bbf1bd9e271dbc60caa0b3d46e5c39b6
anangrybeaver <guitarplayer19997@gmail.com>
2014-05-27 13:35:27 -0400

    Updated Server to Use Brian's

--- SpaceGame/java/src/spacegame/client/ClientMain.java
+++ SpaceGame/java/src/spacegame/client/ClientMain.java
@@ -9,92 +9,91 @@ import javax.swing.JOptionPane;
 import src.spacegame.client.gui.GuiDebug;
 import src.spacegame.client.gui.GuiInGame;
 import src.spacegame.client.gui.GuiMainMenu;
-import src.spacegame.old.Debug;
 import src.spacegame.server.ServerMain;
 import com.arcadeengine.AnimPanel;
 import com.arcadeengine.KeyBinding;
 import com.arcadeengine.gui.TransitionType;
 
 public class ClientMain extends AnimPanel {
-
-	private static final long serialVersionUID = 5863479337524090281L;
-
-	private static final long UPDATE_DELAY = 600;
-
-	private KeyBinding binding;
-
-	private ServerMain locServer;
 	
+	private static final long serialVersionUID = 5863479337524090281L;
+	
+	private static final long UPDATE_DELAY = 600;
+	
+	private KeyBinding binding;
+	
+	private ServerMain locServer;
+
 	private ClientInfo clInfo;
 	private ClientComm clComm;
-
+	
 	private boolean serverRunning = false;
-
+	
 	public ClientMain() {
-
+	
 		this.createInstance("Space Game", 800, 500);
 		this.setResizable(false);
-
+		
 		this.setTimerDelay(60);
-
+		
 		this.initBindings();
-		
+
 		this.getKeyBoardHandler().addBindings(this.binding);
-		
+
 		this.createGuiHandler(new GuiMainMenu(this));
 		this.getGuiHandler().addDebug(new GuiDebug(this));
 		this.getGuiHandler().setDebugState(true);
-
+		
 		clInfo = new ClientInfo(ClientMain.this);
 	}
-	
+
 	@Override
 	public void initRes() {
-
+	
 	}
-	
-	public void initBindings() {
-	
-		binding = new KeyBinding(this) {
 
+	public void initBindings() {
+
+		binding = new KeyBinding(this) {
+			
 			@Override
 			public void singleBinding(String key) {
-			
+
 				if(key.equals("F1"))
 					ClientMain.this.getGuiHandler().invertDebugState();
 			}
 		};
-
+		
 	}
-
+	
 	public ClientInfo getClientInfo() {
-
+	
 		return clInfo;
 	}
-
+	
 	public ClientComm getClientComm() {
-
+	
 		return clComm;
 	}
-
-	public void connectToServer(final String ipAddress) {
 	
-		Thread sv = new Thread() {
+	public void connectToServer(final String ipAddress) {
 
+		Thread sv = new Thread() {
+			
 			@Override
 			public void run() {
-			
+
 				if(ipAddress == null)
 					return;
-
+				
 				if(ipAddress.length() - ipAddress.replace(".", "").length() != 3)
 					return;
-				
+
 				try {
 					Socket socket = new Socket(ipAddress, 4444);
 					ClientMain.this.clComm = new ClientComm(ClientMain.this, socket);
 					ClientMain.this.clInfo.init();
-					
+
 					ClientMain.this.getGuiHandler().switchGui(new GuiInGame(ClientMain.this), TransitionType.fade);
 				}
 				catch(UnknownHostException e) {
@@ -103,35 +102,35 @@ public class ClientMain extends AnimPanel {
 				catch(IOException e) {
 					System.err.println("IOException in method connectToServer in class ClientMain");
 				}
-
+				
 				this.interrupt();
 			}
 		};
-		
+
 		sv.start();
 	}
-
-	public void connectToServer() {
 	
+	public void connectToServer() {
+
 		connectToServer(JOptionPane.showInputDialog("Please enter the Server IP address", ""));
 	}
-
-	public void startServer() {
 	
+	public void startServer() {
+
 		if(serverRunning == false) {
-			
+
 			Thread th = new Thread() {
 				@Override
 				public void run() {
-				
-					Debug.msg("Starting up server...");
+
+					System.out.println("Starting up server...");
 					locServer = new ServerMain();
 					try {
 						serverRunning = true;
 						locServer.runServer();
 					}
 					catch(IOException e) {
-						
+
 						serverRunning = false;
 						e.printStackTrace();
 					}
@@ -140,55 +139,55 @@ public class ClientMain extends AnimPanel {
 			th.start();
 		}
 	}
-
-	public void stopServer() {
 	
+	public void stopServer() {
+
 		if(this.serverRunning)
 			locServer.stop();
-
+		
 		this.serverRunning = false;
 	}
-
-	public boolean isServerRunning() {
 	
+	public boolean isServerRunning() {
+
 		return serverRunning;
 	}
-
+	
 	@Override
 	public Graphics renderFrame(Graphics g) {
-	
+
 		this.calculateRenderFPS();
-		
+
 		this.drawGui(g);
 		return g;
 	}
-
+	
 	@Override
 	public void process() {
-
+	
 		if(System.currentTimeMillis() % UPDATE_DELAY == 0 && clComm != null) {
 			if(clComm.isConnected()) {
 				clComm.addMessage("REQUEST");
 				clComm.sendMessages();
-				
+
 				ArrayList<String> msgs = clComm.getAllMessages();
-				
+
 				for(String msg : msgs) {
 					String header = msg.substring(0, 4);
-					
+
 					System.out.println(header);
 				}
 			}
 		}
-		
+
 		this.updateGui();
 	}
-
-	public void close() {
 	
+	public void close() {
+
 		if(this.clComm != null)
 			clComm.close();
-
+		
 		System.out.println("Closing...");
 		System.exit(0);
 	}
--- SpaceGame/java/src/spacegame/client/gui/GuiInGame.java
+++ SpaceGame/java/src/spacegame/client/gui/GuiInGame.java
@@ -6,51 +6,39 @@ import java.awt.Graphics;
 import src.spacegame.client.ClientMain;
 import com.arcadeengine.AnimPanel;
 import com.arcadeengine.gui.Gui;
-import com.arcadeengine.gui.GuiButton;
 import com.arcadeengine.gui.GuiComponent;
-import com.arcadeengine.gui.TransitionType;
 
 public class GuiInGame extends Gui {
-	
-	ClientMain clMain = (ClientMain) panel;
 
-	private GuiButton back = new GuiButton(panel, 200, 20, "Back");
+	ClientMain clMain = (ClientMain) panel;
 	
 	public GuiInGame(AnimPanel panel) {
-	
+
 		super(panel);
-
-		this.setTitle("In Game Gui Here");
-		this.setTitleColor(new Color(162, 82, 58, 255));
-		this.setTitleFont(new Font("Century Gothic", Font.BOLD, 20));
 		
-		this.setBGColor(81, 41, 21, 200);
-		
-		this.components.add(back);
+		this.setBGColor(0, 0, 0, 0);
 	}
-
+	
 	@Override
 	public void drawGui(Graphics g) {
 	
-		this.drawTitle(g, clMain.getWidth());
-		
 		g.setColor(new Color(48, 48, 48, 150));
 		g.fillRect(0, 0, clMain.getWidth(), 40);
+
+		this.drawString("Player: " + clMain.getClientInfo().getName(), new Font("Arial", Font.BOLD, 12), Color.white, 5, 15, g);
 		
 		this.drawComponents(g, (panel.getWidth() / 2) - 100, 175);
 	}
-	
+
 	@Override
 	public void updateGui() {
-	
+
 		this.updateComponents();
 	}
-	
+
 	@Override
 	public void actionPerformed(GuiComponent comp, int msBtn) {
-
-		if(back.equals(comp))
-			this.clMain.getGuiHandler().previousGui(TransitionType.fade);
-	}
 	
+	}
+
 }
